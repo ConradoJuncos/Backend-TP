f9c06d638a6131cef016fa02a310e92ca2d24759
ConradoJuncos <165090613+ConradoJuncos@users.noreply.github.com>
2024-11-18 00:09:25 -0300

    Logs de EmpleadoService

--- empleados/src/main/java/utn/frc/backend/empleados/service/EmpleadoService.java
+++ empleados/src/main/java/utn/frc/backend/empleados/service/EmpleadoService.java
@@ -1,35 +1,40 @@
 package utn.frc.backend.empleados.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import utn.frc.backend.empleados.exception.EmpleadoNotFoundException;
 import utn.frc.backend.empleados.model.Empleado;
 import utn.frc.backend.empleados.repository.EmpleadoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 public class EmpleadoService {
 
     private final EmpleadoRepository empleadoRepository;
+    private static final Logger logger = LoggerFactory.getLogger(EmpleadoService.class);
 
     @Autowired
     public EmpleadoService(EmpleadoRepository empleadoRepository) {
         this.empleadoRepository = empleadoRepository;
     }
 
-
-    // todo - Revisar esto. En el peor caso, busca si el id ya existe, lo cual nunca pasa porque el id se gestiona automaticamente y es inutil
-
     public Empleado crearEmpleado(Empleado empleado) {
+        logger.info("Intentando crear un empleado con legajo: {}", empleado.getLegajo());
         if (empleadoRepository.existsById(empleado.getLegajo())) {
+            logger.warn("El empleado con legajo {} ya existe. Lanzando excepción.", empleado.getLegajo());
             throw new IllegalArgumentException("El empleado ya existe");
         }
+        logger.info("Empleado creado exitosamente con legajo: {}", empleado.getLegajo());
         return empleadoRepository.save(empleado);
     }
 
     public Empleado obtenerEmpleadoPorId(Long id) {
+        logger.info("Buscando empleado con ID: {}", id);
         return empleadoRepository.findById(id)
-                .orElseThrow(() -> new EmpleadoNotFoundException("Empleado no encontrado"));
+                .orElseThrow(() -> {
+                    logger.warn("Empleado con ID {} no encontrado. Lanzando excepción.", id);
+                    return new EmpleadoNotFoundException("Empleado no encontrado");
+                });
     }
 }
