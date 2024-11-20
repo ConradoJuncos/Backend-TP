9a0442e062a06ec48ccd41a884d1474362013fe8
ConradoJuncos <165090613+ConradoJuncos@users.noreply.github.com>
2024-11-20 17:05:28 -0300

    Agregadas rutas al gateway. No puedo acceder a rutas protegidas por el gateway, error 403. Me parece que es un error de roles porque si saco los roles, funciona bien

--- gateway/src/main/java/utn/frc/backend/gateway/config/SecurityConfig.java
+++ gateway/src/main/java/utn/frc/backend/gateway/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers;
 
@@ -13,27 +14,26 @@ import org.springframework.security.web.server.util.matcher.ServerWebExchangeMat
 public class SecurityConfig {
 
     // Las rutas específicas tienen prioridad sobre las generales
-    // /api/notificaciones/nueva se evalua antes que /api/notificaciones, entonces un empleado puede
-    //   crear una nueva notificacion sin ser admin
+    // /api/notificaciones/nueva se evalúa antes que /api/notificaciones, entonces un empleado puede
+    //   crear una nueva notificación sin ser admin
 
-//    Obtencion de un token con Postman:
+//    Obtención de un token con Postman:
 //    POST a https://labsys.frc.utn.edu.ar/aim/realms/backend-tps/protocol/openid-connect/token
 //    Token Name: tokenName
 //    Grant Type: Password Credentials
 //    Access Token URL: https://labsys.frc.utn.edu.ar/aim/realms/backend-tps/protocol/openid-connect/token
 //    Client ID: cliente-tpi
-//    Client Sectet: ZmiMMce6zh4xKHde1FFKbfkiThDyYpyn
+//    Client Secret: ZmiMMce6zh4xKHde1FFKbfkiThDyYpyn
 //    Username: g148-A
-//    Password: g148-B-backend
+//    Password: g148-A-backend
 //    Client Authentication: Send as basic auth header
-//    Despues hago la request con Bearer Token = el token que consigo
+//    Después hago la request con Bearer Token = el token que consigo
 
 
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         http
                 .authorizeExchange(authorize -> authorize
-
                         // Rutas específicas para Empleado
                         .matchers(ServerWebExchangeMatchers.pathMatchers("/api/pruebas/crear")).hasRole("Empleado")
                         .matchers(ServerWebExchangeMatchers.pathMatchers("/api/notificaciones/crear")).hasRole("Empleado")
@@ -44,12 +44,17 @@ public class SecurityConfig {
                         // Rutas específicas para Admin
                         .matchers(ServerWebExchangeMatchers.pathMatchers("/api/notificaciones")).hasRole("Admin")
                         .matchers(ServerWebExchangeMatchers.pathMatchers("/api/notificaciones/empleado/{idEmpleado}")).hasRole("Admin")
+                        .matchers(ServerWebExchangeMatchers.pathMatchers("/api/posiciones/kilometros")).hasRole("Admin")
                         .matchers(ServerWebExchangeMatchers.pathMatchers("/api/pruebas/vehiculo/{idVehiculo}")).hasRole("Admin")
 
                         // Rutas protegidas por defecto
                         .anyExchange().authenticated()
                 )
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+                .oauth2ResourceServer(oauth2 ->
+                        oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(
+                                new ReactiveJwtAuthenticationConverterAdapter(jwtAuthenticationConverter())
+                        ))
+                );
         return http.build();
     }
 
@@ -62,6 +67,7 @@ public class SecurityConfig {
 
         JwtAuthenticationConverter authenticationConverter = new JwtAuthenticationConverter();
         authenticationConverter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesConverter);
+
         return authenticationConverter;
     }
 }
